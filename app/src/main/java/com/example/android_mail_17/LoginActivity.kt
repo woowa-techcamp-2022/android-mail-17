da5bc58389227dba94018760fcb6dd8563420dba
Ji <hwichance@gmail.com>
2022-07-06 17:52:17 +0900

    :sparkles: #1 feat: 닉네임과 이메일 조건 만족 여부에 따라 버튼 활성화 구현
    - inputType 설정
    - LiveData로 닉네임과 이메일 조건 만족 여부를 관리
    - Observer를 통해 LiveData를 관찰하고 조건 만족 여부에 따라 버튼 활성화

--- app/src/main/java/com/example/android_mail_17/LoginActivity.kt
+++ app/src/main/java/com/example/android_mail_17/LoginActivity.kt
@@ -3,13 +3,18 @@ package com.example.android_mail_17
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.android_mail_17.databinding.ActivityLoginBinding
+import com.example.android_mail_17.models.ConstraintFlag
 import com.example.android_mail_17.others.Utils
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
-    private var _binding : ActivityLoginBinding? = null
-    private val binding: ActivityLoginBinding get() = requireNotNull(_binding)
+    private var _binding: ActivityLoginBinding? = null
+    val binding: ActivityLoginBinding get() = requireNotNull(_binding)
+    private var _constraintFlag = MutableLiveData<ConstraintFlag>()
+    val constraintFlag: LiveData<ConstraintFlag> get() = _constraintFlag
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,21 +22,42 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setTextChangeListener()
+        setLiveDataObserver()
     }
 
     private fun setTextChangeListener() {
         binding.loginNickNameEditText.addTextChangedListener {
+            val emailFlag = _constraintFlag.value?.emailConstraintFlag ?: false
             when {
-                it.isNullOrBlank() -> resetError(binding.loginNickNameLayout)
-                Utils.checkNicknameConstraint(it.toString()) -> resetError(binding.loginNickNameLayout)
-                else -> setNicknameError(binding.loginNickNameLayout)
+                it.isNullOrBlank() -> {
+                    resetError(binding.loginNickNameLayout)
+                    setFlag(false, emailFlag)
+                }
+                Utils.checkNicknameConstraint(it.toString()) -> {
+                    resetError(binding.loginNickNameLayout)
+                    setFlag(true, emailFlag)
+                }
+                else -> {
+                    setNicknameError(binding.loginNickNameLayout)
+                    setFlag(false, emailFlag)
+                }
             }
         }
         binding.loginEmailEditText.addTextChangedListener {
+            val nicknameFlag = _constraintFlag.value?.nicknameConstraintFlag ?: false
             when {
-                it.isNullOrBlank() -> resetError(binding.loginEmailLayout)
-                Utils.checkEmailConstraint(it.toString()) -> resetError(binding.loginEmailLayout)
-                else -> setEmailError(binding.loginEmailLayout)
+                it.isNullOrBlank() -> {
+                    resetError(binding.loginEmailLayout)
+                    setFlag(nicknameFlag, false)
+                }
+                Utils.checkEmailConstraint(it.toString()) -> {
+                    resetError(binding.loginEmailLayout)
+                    setFlag(nicknameFlag, true)
+                }
+                else -> {
+                    setEmailError(binding.loginEmailLayout)
+                    setFlag(nicknameFlag, false)
+                }
             }
         }
     }
@@ -46,5 +72,15 @@ class LoginActivity : AppCompatActivity() {
 
     private fun setEmailError(textInputLayout: TextInputLayout) {
         textInputLayout.error = getString(R.string.email_error)
+    }
+
+    private fun setFlag(nicknameFlag: Boolean, emailFlag: Boolean) {
+        _constraintFlag.postValue(ConstraintFlag(nicknameFlag, emailFlag))
+    }
+
+    private fun setLiveDataObserver() {
+        constraintFlag.observe(this) { flag ->
+            binding.loginButton.isEnabled = flag.nicknameConstraintFlag && flag.emailConstraintFlag
+        }
     }
 }
---
+++ app/src/main/java/com/example/android_mail_17/models/ConstraintFlag.kt
@@ -0,0 +1,3 @@
+package com.example.android_mail_17.models
+
+data class ConstraintFlag(val nicknameConstraintFlag: Boolean, val emailConstraintFlag: Boolean)
