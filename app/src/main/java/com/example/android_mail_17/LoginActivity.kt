6e7096b9da09ff1487ffb2e9ac7b4faa845a2f4d
Ji <hwichance@gmail.com>
2022-07-06 16:23:26 +0900

    :sparkles: #1 feat: loginActivity binding과 닉네임, 이메일 입력 조건 검사 구현 - viewBinding 사용 - addTextChangedListener로 입력 변화 감지 후 조건 검사 수행 - utility object 생성 후 문자열 패턴 매칭 메소드 구현

--- app/src/main/java/com/example/android_mail_17/LoginActivity.kt
+++ app/src/main/java/com/example/android_mail_17/LoginActivity.kt
@@ -2,10 +2,49 @@ package com.example.android_mail_17
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import com.example.android_mail_17.databinding.ActivityLoginBinding
+import com.example.android_mail_17.others.Utils
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    private var _binding : ActivityLoginBinding? = null
+    private val binding: ActivityLoginBinding get() = requireNotNull(_binding)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        _binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setTextChangeListener()
+    }
+
+    private fun setTextChangeListener() {
+        binding.loginNickNameEditText.addTextChangedListener {
+            when {
+                it.isNullOrBlank() -> resetError(binding.loginNickNameLayout)
+                Utils.checkNicknameConstraint(it.toString()) -> resetError(binding.loginNickNameLayout)
+                else -> setNicknameError(binding.loginNickNameLayout)
+            }
+        }
+        binding.loginEmailEditText.addTextChangedListener {
+            when {
+                it.isNullOrBlank() -> resetError(binding.loginEmailLayout)
+                Utils.checkEmailConstraint(it.toString()) -> resetError(binding.loginEmailLayout)
+                else -> setEmailError(binding.loginEmailLayout)
+            }
+        }
+    }
+
+    private fun resetError(textInputLayout: TextInputLayout) {
+        textInputLayout.error = null
+    }
+
+    private fun setNicknameError(textInputLayout: TextInputLayout) {
+        textInputLayout.error = getString(R.string.nickname_error)
+    }
+
+    private fun setEmailError(textInputLayout: TextInputLayout) {
+        textInputLayout.error = getString(R.string.email_error)
     }
 }
