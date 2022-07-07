cf88e0d762b7bce7f0f5b4f18adef8d1438ebd6a
Ji <hwichance@gmail.com>
2022-07-07 15:48:09 +0900

    :sparkles: #5 feat: setting 화면에 닉네임과 이메일 표시 구현
    - loginActivity에서 putExtra method로 입력 받은 정보를 homeActivity로 전달
    - 전달받은 정보는 viewModel을 통해 관리
    - setting fragment에서 viewModel 속 입력 정보를 가져와서 화면에 출력

--- app/src/main/java/com/example/android_mail_17/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/example/android_mail_17/ui/activities/HomeActivity.kt
@@ -2,21 +2,29 @@ package com.example.android_mail_17.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.example.android_mail_17.R
 import com.example.android_mail_17.databinding.ActivityHomeBinding
 import com.example.android_mail_17.ui.fragments.MailFragment
 import com.example.android_mail_17.ui.fragments.SettingFragment
+import com.example.android_mail_17.viewmodels.InputViewModel
 
 class HomeActivity : AppCompatActivity() {
     private var _binding: ActivityHomeBinding? = null
     private val binding: ActivityHomeBinding get() = requireNotNull(_binding)
+    private val inputViewModel by viewModels<InputViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        intent.extras?.run {
+            inputViewModel.saveNickname(getString("nickname", "nickname"))
+            inputViewModel.saveEmail(getString("email", "email"))
+        }
 
         setAppBarMenuClickListener()
         setDrawerItemClickListener()
--- app/src/main/java/com/example/android_mail_17/ui/fragments/SettingFragment.kt
+++ app/src/main/java/com/example/android_mail_17/ui/fragments/SettingFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.android_mail_17.R
 import com.example.android_mail_17.databinding.FragmentSettingBinding
 import com.example.android_mail_17.ui.activities.HomeActivity
+import com.example.android_mail_17.viewmodels.InputViewModel
 
 class SettingFragment : Fragment() {
     private var _binding: FragmentSettingBinding? = null
     private val binding: FragmentSettingBinding get() = requireNotNull(_binding)
+    private val inputViewModel by activityViewModels<InputViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,10 +28,16 @@ class SettingFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setAppBar()
+        setInformation()
     }
 
     private fun setAppBar() {
         (activity as HomeActivity).setAppBar(null, R.string.setting)
+    }
+
+    private fun setInformation() {
+        binding.nickNameView.text = inputViewModel.nickname
+        binding.emailView.text = inputViewModel.email
     }
 
     override fun onDestroyView() {
